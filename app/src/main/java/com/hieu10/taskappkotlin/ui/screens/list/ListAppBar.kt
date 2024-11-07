f45869eb2c505d6e7f32b6e84486dc4edb4d88bc
DoTrungHieu2004 <hieu10productions@gmail.com>
2024-11-07 10:37:29 +0700

    Add some string resources

--- app/src/main/java/com/hieu10/taskappkotlin/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/hieu10/taskappkotlin/ui/screens/list/ListAppBar.kt
@@ -86,7 +86,7 @@ fun DefaultListAppBar(
     TopAppBar (
         title = {
             Text(
-                text = "Tasks",
+                text = stringResource(id = R.string.list_screen_title),
                 color = MaterialTheme.colors.topAppBarContentColor
             )
         },
@@ -223,7 +223,7 @@ fun SearchAppBar(
             modifier = Modifier.fillMaxWidth(),
             placeholder = {
                 Text(
-                    text = "Search...",
+                    text = stringResource(id = R.string.search_placeholder),
                     color = Color.White,
                     modifier = Modifier.alpha(ContentAlpha.medium)
                 )
@@ -240,7 +240,7 @@ fun SearchAppBar(
                 ) {
                     Icon(
                         imageVector = Icons.Filled.Search,
-                        contentDescription = "Search icon",
+                        contentDescription = stringResource(id = R.string.search_icon),
                         tint = MaterialTheme.colors.topAppBarContentColor
                     )
                 }
@@ -266,7 +266,7 @@ fun SearchAppBar(
                 ) {
                     Icon(
                         imageVector = Icons.Filled.Close,
-                        contentDescription = "CLose icon",
+                        contentDescription = stringResource(id = R.string.close_icon),
                         tint = MaterialTheme.colors.topAppBarContentColor
                     )
                 }
