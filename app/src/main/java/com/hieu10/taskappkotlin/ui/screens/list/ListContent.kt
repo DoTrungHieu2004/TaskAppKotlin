10663bd6a5472df29d74671bdddc446ada0d030f
DoTrungHieu2004 <hieu10productions@gmail.com>
2024-11-07 10:38:37 +0700

    Design task item

---
+++ app/src/main/java/com/hieu10/taskappkotlin/ui/screens/list/ListContent.kt
@@ -0,0 +1,101 @@
+package com.hieu10.taskappkotlin.ui.screens.list
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.hieu10.taskappkotlin.data.models.Priority
+import com.hieu10.taskappkotlin.data.models.Task
+import com.hieu10.taskappkotlin.ui.theme.LARGE_PADDING
+import com.hieu10.taskappkotlin.ui.theme.PRIORITY_INDICATOR_SIZE
+import com.hieu10.taskappkotlin.ui.theme.TASK_ITEM_ELEVATION
+import com.hieu10.taskappkotlin.ui.theme.taskItemBackgroundColor
+import com.hieu10.taskappkotlin.ui.theme.taskItemTextColor
+
+@Composable
+fun ListContent() {
+
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun TaskItem(
+    task: Task,
+    navigateToTaskScreen: (taskId: Int) -> Unit
+) {
+    Surface (
+        modifier = Modifier
+            .fillMaxWidth(),
+        color = MaterialTheme.colors.taskItemBackgroundColor,
+        shape = RectangleShape,
+        elevation = TASK_ITEM_ELEVATION,
+        onClick = {
+            navigateToTaskScreen(task.id)
+        }
+    ) {
+        Column (
+            modifier = Modifier
+                .padding(all = LARGE_PADDING)
+                .fillMaxWidth()
+        ) {
+            Row {
+                Text(
+                    text = task.title,
+                    color = MaterialTheme.colors.taskItemTextColor,
+                    style = MaterialTheme.typography.h5,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    modifier = Modifier.weight(8f)
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    contentAlignment = Alignment.TopEnd
+                ) {
+                    Canvas(
+                        modifier = Modifier
+                            .width(PRIORITY_INDICATOR_SIZE)
+                            .height(PRIORITY_INDICATOR_SIZE)
+                    ) {
+                        drawCircle(
+                            color = task.priority.color
+                        )
+                    }
+                }
+            }
+            Text(
+                text = task.description,
+                modifier = Modifier.fillMaxWidth(),
+                color = MaterialTheme.colors.taskItemTextColor,
+                style = MaterialTheme.typography.subtitle1,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun TaskItemPreview() {
+    TaskItem(
+        task = Task(0, "Title", "Some random text", Priority.MEDIUM),
+        navigateToTaskScreen = {}
+    )
+}
